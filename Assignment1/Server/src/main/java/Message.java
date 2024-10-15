55624a8d32fb8f976f8db7e736794ee22dbdd43d
Weinan Shi <shi.wein@northeastern.edu>
2024-10-14 22:47:57 -0700

    Initial commit

---
+++ Assignment1/Server/src/main/java/Message.java
@@ -0,0 +1,14 @@
+// Static nested class for Message
+public class Message {
+    private String message;
+
+    public Message() {}
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
